8287c80a3db9861604b7894665b7d8dd96a0883d
marasinipradeep <marasinipradeep@gmail.com>
2020-12-23 20:47:48 +1030

    #Updatting permissions in local state completed

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8e9f33a92df713b8dcbc";
+/******/ 	var hotCurrentHash = "ce2792cf6e999a0f9898";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/public/components/Permissions.js
+++ frontend/public/components/Permissions.js
@@ -1,86 +1,131 @@
-import {Query} from 'react-apollo';
-import Error from '../components/ErrorMessage';
+import { Query, Mutation } from 'react-apollo';
+import Error from './ErrorMessage';
 import gql from 'graphql-tag';
 import Table from './styles/Table';
-import SickButton from "../components/styles/SickButton";
+import SickButton from './styles/SickButton';
+import PropTypes from 'prop-types';
 
 const possiblePermissions = [
-    'ADMIN',
-    'USER',
-    'ITEMCREATE',
-    'ITEMUPDATE',
-    'ITEMDELETE',
-    'PERMISSIONUPDATE',
+  'ADMIN',
+  'USER',
+  'ITEMCREATE',
+  'ITEMUPDATE',
+  'ITEMDELETE',
+  'PERMISSIONUPDATE',
 ];
 
-const ALL_USERS_QUERY = gql `
-
-query {
-
-    users{
-        id
-        name
-        email
-        permissions
+const UPDATE_PERMISSIONS_MUTATION = gql`
+  mutation updatePermissions($permissions: [Permission], $userId: ID!) {
+    updatePermissions(permissions: $permissions, userId: $userId) {
+      id
+      permissions
+      name
+      email
     }
-}
+  }
+`;
 
-`
+const ALL_USERS_QUERY = gql`
+  query {
+    users {
+      id
+      name
+      email
+      permissions
+    }
+  }
+`;
 
-const Permissions = (props)=>(
+const Permissions = props => (
+  <Query query={ALL_USERS_QUERY}>
+    {({ data, loading, error }) => (
+      <div>
+        <Error error={error} />
+        <div>
+          <h2>Manage Permissions</h2>
+          <Table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                {possiblePermissions.map(permission => <th key={permission}>{permission}</th>)}
+                <th>👇🏻</th>
+              </tr>
+            </thead>
+            <tbody>{data.users && data.users.map(user => <UserPermissions user={user} key={user.id} />)}</tbody>
+          </Table>
+        </div>
+      </div>
+    )}
+  </Query>
+);
 
-    <Query query={ALL_USERS_QUERY}>
-
-        {({data,loading,error})=>(
-            <div>
-            <Error error={error}/>
-
-            <div>
-                <h2>Manage Permission</h2>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            {possiblePermissions.map(permission=><th>{permission}</th>)}
-                            <th>🤞 </th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.users.map(user=><User user={user}/>)}
-                    </tbody>
-                </Table>
-            </div>
-
-            </div>
-
-        )}
-
-    </Query>
-)
-
-class User extends React.Component{
-    render(){
-        const user = this.props.user;
-        return(
-            <tr>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                {possiblePermissions.map(permission=>(
-                    <td>
-                        <label htmlFor={`${user.id}-permission-${permission}`}>
-                            <input type="checkbox"/>
-
-                        </label>
-                    </td>
-                ))}
-                <td>
-                    <SickButton>UPDATE</SickButton>
-
+class UserPermissions extends React.Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      email: PropTypes.string,
+      id: PropTypes.string,
+      permissions: PropTypes.array,
+    }).isRequired,
+  };
+  state = {
+    permissions: this.props.user.permissions,
+  };
+  handlePermissionChange = (e) => {
+    const checkbox = e.target;
+    // take a copy of the current permissions
+    let updatedPermissions = [...this.state.permissions];
+    // figure out if we need to remove or add this permission
+    if (checkbox.checked) {
+      // add it in!
+      updatedPermissions.push(checkbox.value);
+    } else {
+      updatedPermissions = updatedPermissions.filter(permission => permission !== checkbox.value);
+    }
+    this.setState({ permissions: updatedPermissions });
+  };
+  render() {
+    const user = this.props.user;
+    return (
+      <Mutation
+        mutation={UPDATE_PERMISSIONS_MUTATION}
+        variables={{
+          permissions: this.state.permissions,
+          userId: this.props.user.id,
+        }}
+      >
+        {(updatePermissions, { loading, error }) => (
+          <>
+            {error && <tr><td colspan="8"><Error error={error} /></td></tr>}
+            < tr >
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              {possiblePermissions.map(permission => (
+                <td key={permission}>
+                  <label htmlFor={`${user.id}-permission-${permission}`}>
+                    <input
+                      id={`${user.id}-permission-${permission}`}
+                      type="checkbox"
+                      checked={this.state.permissions.includes(permission)}
+                      value={permission}
+                      onChange={this.handlePermissionChange}
+                    />
+                  </label>
                 </td>
+              ))}
+              <td>
+                <SickButton type="button" disabled={loading} onClick={updatePermissions}>
+                  Updat{loading ? 'ing' : 'e'}
+                </SickButton>
+              </td>
             </tr>
+          </>
         )
-    }
+        }
+      </Mutation>
+    );
+  }
 }
+
 export default Permissions;
